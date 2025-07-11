e0d04f5175134f7a425ff05fe1103b8c33971091
deepkumar05 <ddeepanshu2088@gmail.com>
2025-07-11 12:17:56 +0530

    Bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider 
     attribute="class" 
-    defaultTheme="root" 
+    defaultTheme="light" 
     enableSystem
     disableTransitionOnChange
   >
